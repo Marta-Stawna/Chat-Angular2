81b848cc02de35ec39b46ef4a26a0bea5f1117d2
Marta <martastawna22@gmail.com>
2017-03-28 16:08:22 +0200

    create-keyeords-list

--- src/app/chat/chat-room/chat-room.service.ts
+++ src/app/chat/chat-room/chat-room.service.ts
@@ -4,5 +4,16 @@ import { Injectable } from '@angular/core';
 export class ChatRoomService {
 
   constructor() { }
+  public key_words_data=[
+    "Juventus Turyn",
+    "SS Lazio Rzym",
+    "Liga włoska",
+    "Gonzalo Higuain",
+    "Mario Mandzukić"
+  ]
+
+  getKeyWords(){
+    return this.key_words_data;
+  }
 
 }
--- src/app/chat/chat-room/key-words-list/key-words-list.component.ts
+++ src/app/chat/chat-room/key-words-list/key-words-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {ChatRoomService} from '../chat-room.service';
 
 @Component({
   selector: 'app-key-words-list',
   templateUrl: './key-words-list.component.html',
-  styleUrls: ['./key-words-list.component.css']
+  styleUrls: ['./key-words-list.component.css'],
+  providers:[ChatRoomService]
 })
 export class KeyWordsListComponent implements OnInit {
 
-  constructor() { }
+  public key_words;
+
+  constructor(private serviceChatRoom:ChatRoomService ) { }
 
   ngOnInit() {
+    this.key_words=this.serviceChatRoom.getKeyWords();
+    console.log(this.key_words);
   }
 
 }
