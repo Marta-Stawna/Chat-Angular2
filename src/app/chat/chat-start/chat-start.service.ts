0d82c81749d7ea8486d07ec313241926ae2eec4b
katdon <katdon@st.amu.edu.pl>
2017-04-26 02:19:28 +0200

    change-themes-list

--- src/app/chat/chat-start/chat-start.service.ts
+++ src/app/chat/chat-start/chat-start.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/observable';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ChatStartService {
 
-  constructor() { }
+  constructor(private http: Http) { }
+
+  getThemes() : Observable<string[]>{
+    return this.http.get('src/app/chat/data/themesData.json')
+      .map((res)=> res.json().data);
+  }
 
 }
--- src/app/chat/chat-start/chat-themes/chat-themes.component.ts
+++ src/app/chat/chat-start/chat-themes/chat-themes.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatStartService } from '../chat-start.service';
+import { Subscription } from "rxjs/Subscription";
 
 @Component({
   selector: 'app-chat-themes',
   templateUrl: './chat-themes.component.html',
-  styleUrls: ['./chat-themes.component.css']
+  styleUrls: ['./chat-themes.component.css'],
+  providers: [ChatStartService]
 })
 export class ChatThemesComponent implements OnInit {
 
-  constructor() { }
+  public themes:Array<string> = [];
+
+
+  constructor(private chatStartService: ChatStartService) { }
 
   ngOnInit() {
+    this.chatStartService.getThemes().subscribe(
+      data => this.themes = data
+    );
   }
+
+
 
 }
